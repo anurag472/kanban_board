121d5fe2c54cd4cf8abec3b16bd00517062ee688
anurag47 <anurag.patil@coradius.io>
2023-10-21 19:46:08 +0530

    added local storage and refactored code

--- script.js
+++ script.js
@@ -13,6 +13,14 @@ let isDeleteModeActive = false
 let isEditingCardContent = false
 const colorsArray = ['lightpink', 'lightgreen', 'lightblue', 'black']
 const ticketsArray = []
+const LOCALSTORAGE_KEY = 'tickets'
+const ticketsInStorage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))
+
+if (ticketsInStorage) {
+    ticketsInStorage.forEach(ticket => {
+        createTask(ticket.type, ticket.value, ticket.id)
+    })
+}
 
 addbtn.addEventListener('click', () => {
     if (isModalTaskOpen) {
@@ -24,17 +32,6 @@ addbtn.addEventListener('click', () => {
         isModalTaskOpen = true
         mainContainer.style.opacity = '0.4'
     }
-})
-
-
-allPriorityColors.forEach((container) => {
-    container.addEventListener('click', () => {
-        allPriorityColors.forEach((container) => {
-            container.classList.remove('active')
-        })
-        container.classList.add('active')
-        currentPriorityColor = container.classList[0]
-    })
 })
 
 modal.addEventListener('keydown', (event) => {
@@ -55,84 +52,6 @@ modal.addEventListener('keydown', (event) => {
     }
 })
 
-function createTask(taskType, textValue, taskId) {
-    const newDiv = document.createElement('div')
-    newDiv.classList.add('ticket-cont')
-
-    newDiv.innerHTML = `<div class="ticket-color ${taskType}"></div>
-    <div class="ticket-id">Task Id - ${taskId}</div>
-    <div class="task-area">${textValue}</div>
-    <div class="ticket-lock">
-        <i class="fa-solid fa-lock"></i>
-    </div>`
-
-    const foundTicketIndex = ticketsArray.findIndex(ele => {
-        return ele.id == taskId
-    })
-    if (foundTicketIndex == -1) {
-        const ticketObject = {
-            id: taskId,
-            type: taskType,
-            value: textValue
-        }
-        ticketsArray.push(ticketObject)
-    }
-
-    handlePriorityChange(newDiv, taskId)
-    handleLock(newDiv, taskId)
-    handleRemoval(newDiv)
-    mainContainer.appendChild(newDiv)
-}
-
-function handlePriorityChange(div,ticketId) {
-    const coloredDiv = div.querySelector('.ticket-color')
-
-    const idx = findTicketIdx(ticketId)
-
-    coloredDiv.addEventListener('click', event => {
-        const currentColor = coloredDiv.classList[1]
-        const index = colorsArray.findIndex(ele => ele === currentColor)
-        const newIndex = (index + 1) % colorsArray.length
-        coloredDiv.classList.remove(currentColor)
-        coloredDiv.classList.add(colorsArray[newIndex])
-        ticketsArray[idx].type = colorsArray[newIndex]
-    })
-}
-
-function findTicketIdx(id){
-    return ticketsArray.findIndex(ele => {
-        return ele.id == id
-    })
-}
-
-function handleLock(div, ticketId) {
-    const lock = div.querySelector('.ticket-lock > i')
-    const taskAreaElement = div.querySelector('.task-area')
-    const idx = findTicketIdx(ticketId)
-    lock.addEventListener('click', event => {
-        if (!isEditingCardContent) {
-            lock.classList.remove('fa-lock')
-            lock.classList.add('fa-lock-open')
-            taskAreaElement.setAttribute('contenteditable', 'true')
-            taskAreaElement.focus()
-        } else {
-            lock.classList.add('fa-lock')
-            lock.classList.remove('fa-lock-open')
-            taskAreaElement.setAttribute('contenteditable', 'false')
-            ticketsArray[idx].value = taskAreaElement.innerText
-        }
-        isEditingCardContent = !isEditingCardContent
-    })
-}
-
-function handleRemoval(divToBeRemoved) {
-    divToBeRemoved.addEventListener('click', event => {
-        if (isDeleteModeActive) {
-            divToBeRemoved.remove()
-        }
-    })
-}
-
 removebtn.addEventListener('click', () => {
     isDeleteModeActive = !isDeleteModeActive
     if (isDeleteModeActive) {
@@ -142,6 +61,16 @@ removebtn.addEventListener('click', () => {
         alert('Delete mode has been de-activated')
         removebtn.style.color = 'white'
     }
+})
+
+allPriorityColors.forEach((container) => {
+    container.addEventListener('click', () => {
+        allPriorityColors.forEach((container) => {
+            container.classList.remove('active')
+        })
+        container.classList.add('active')
+        currentPriorityColor = container.classList[0]
+    })
 })
 
 allFiltercontainers.forEach(container => {
@@ -171,3 +100,87 @@ allFiltercontainers.forEach(container => {
         })
     })
 })
+
+function createTask(taskType, textValue, taskId) {
+    const newDiv = document.createElement('div')
+    newDiv.classList.add('ticket-cont')
+
+    newDiv.innerHTML = `<div class="ticket-color ${taskType}"></div>
+    <div class="ticket-id">Task Id - ${taskId}</div>
+    <div class="task-area">${textValue}</div>
+    <div class="ticket-lock">
+        <i class="fa-solid fa-lock"></i>
+    </div>`
+
+    const foundTicketIndex = ticketsArray.findIndex(ele => {
+        return ele.id == taskId
+    })
+    if (foundTicketIndex == -1) {
+        const ticketObject = {
+            id: taskId,
+            type: taskType,
+            value: textValue
+        }
+        ticketsArray.push(ticketObject)
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(ticketsArray))
+    }
+
+    handlePriorityChange(newDiv, taskId)
+    handleLock(newDiv, taskId)
+    handleRemoval(newDiv, taskId)
+    mainContainer.appendChild(newDiv)
+}
+
+function handlePriorityChange(div, ticketId) {
+    const coloredDiv = div.querySelector('.ticket-color')
+
+    const idx = findTicketIdx(ticketId)
+
+    coloredDiv.addEventListener('click', event => {
+        const currentColor = coloredDiv.classList[1]
+        const index = colorsArray.findIndex(ele => ele === currentColor)
+        const newIndex = (index + 1) % colorsArray.length
+        coloredDiv.classList.remove(currentColor)
+        coloredDiv.classList.add(colorsArray[newIndex])
+        ticketsArray[idx].type = colorsArray[newIndex]
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(ticketsArray))
+    })
+}
+
+function findTicketIdx(id) {
+    return ticketsArray.findIndex(ele => {
+        return ele.id == id
+    })
+}
+
+function handleLock(div, ticketId) {
+    const lock = div.querySelector('.ticket-lock > i')
+    const taskAreaElement = div.querySelector('.task-area')
+    const idx = findTicketIdx(ticketId)
+    lock.addEventListener('click', event => {
+        if (!isEditingCardContent) {
+            lock.classList.remove('fa-lock')
+            lock.classList.add('fa-lock-open')
+            taskAreaElement.setAttribute('contenteditable', 'true')
+            taskAreaElement.focus()
+        } else {
+            lock.classList.add('fa-lock')
+            lock.classList.remove('fa-lock-open')
+            taskAreaElement.setAttribute('contenteditable', 'false')
+            ticketsArray[idx].value = taskAreaElement.innerText
+            localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(ticketsArray))
+        }
+        isEditingCardContent = !isEditingCardContent
+    })
+}
+
+function handleRemoval(divToBeRemoved, ticketId) {
+    const idx = findTicketIdx(ticketId)
+    divToBeRemoved.addEventListener('click', event => {
+        if (isDeleteModeActive) {
+            divToBeRemoved.remove()
+            ticketsArray.splice(idx, 1)
+            localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(ticketsArray))
+        }
+    })
+}
